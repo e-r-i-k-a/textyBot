9caace71f81f36a4adb595d021c9beb05f562721
e-r-i-k-a <samuels.erika@yahoo.com>
2019-09-22 23:02:37 -0400

    deploy init

--- db/index.js
+++ db/index.js
@@ -1,6 +1,27 @@
 const sequelize = require('sequelize');
+const database = process.env.SQL_DATABASE;
+const username = process.env.SQL_USER;
+const password = process.env.SQL_PASSWORD;
+const connection = process.env.INSTANCE_CONNECTION_NAME;
+const host = connection ? `/cloudsql/${connection}` : 'localhost';
 
-module.exports = new sequelize(process.env.DATABASE_NAME || ('textybot', {
+module.exports = new sequelize(database || ('textybot', {
   dialect: 'postgres',
-  host: 'localhost',
+  host,
 }));
+
+// module.epxports = new sequelize(database, username, password, {
+//   dialect: 'postgres',
+//   host,
+//   pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//   },
+//   dialectOptions: {
+//       socketPath: host,
+//   },
+//   logging: false,
+//   operatorsAliases: false
+// });
