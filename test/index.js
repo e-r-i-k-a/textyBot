b58314af5aa2e3e31b53b56f739b69dc4400c0ef
e-r-i-k-a <samuels.erika@yahoo.com>
2019-09-22 23:05:12 -0400

    tests init

--- test/index.js
+++ test/index.js
@@ -1,40 +1,31 @@
 const {expect} = require('chai');
 const assert = require('assert');
 
-beforeEach('Setting up the userList', () => {
+beforeEach('setup the test env', () => {
   console.log('beforeEach');
 });
 
 describe('test init', () => {
-  it ('should init', () => {
+  it ('test1', () => {
     assert.equal("hello".length, 5);
-  })
-});
-
-describe('should send a text', () => {
-  it ('composes a text in twilio', () => {
-    //...
   });
-  it ('composes a text in twilio', () => {
-    //...
-  });
-  it('should return true if valid user id', function(){
+  it('test2', () => {
     var isValid = true;
     expect(isValid).to.be.true;
   });
-})
+});
 
-// function isValidUserIdAsync(userList, user, callback) {
-//   setTimeout(function(){
-//     callback(userList.indexOf(user) >= 0)
-//   }, 1);
-// }
-// Note: setTimeout has been used to simulate the async behavior.
-// /* Test */
-// it('should return true if valid user id', function(done){
-// loginController.isValidUserIdAsync(['abc123','xyz321'], 'abc123',
-//    function(isValid){
-//     assert.equal(isValid, true);
-//     done();
-// });
-// });
+describe('should send a text', () => {
+  it ('composes a text', () => {
+    //...
+  });
+  it ('sends a text', () => {
+    //...
+  });
+  it ('sent at the right time', () => {
+    //...
+  });
+  it ('updates the DB', () => {
+    //...
+  });
+})
